f8fa8013329ffa54bae9674c66d90e11851d4d68
Aaron Enyetu <aaronenyetu@gmail.com>
2023-02-07 00:48:44 -0800

    turned of configuration

--- assets/vendor/bootstrap/js/bootstrap.bundle.js
+++ assets/vendor/bootstrap/js/bootstrap.bundle.js
@@ -12,10 +12,10 @@
             (global = global || self, factory(global.bootstrap = {}, global.jQuery));
 }(this, (function(exports, $) { 'use strict';
 
-   $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
+$ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
-    function _defineProperties(target, props) {
-        for (var i = 0; i < props.length; i++) {
+function _defineProperties(target, props) {
+for (var i = 0; i < props.length; i++) {
             var descriptor = props[i];
             descriptor.enumerable = descriptor.enumerable || false;
             descriptor.configurable = true;
